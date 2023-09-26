17b979d00f9f6ef3319a2ba2d341dc64247b46ac
Imamsaa <imamsafii2222022@gmai.com>
2023-09-26 09:30:24 +0700

    Berhasil membuat menu laporan

--- app/Controllers/Admin/Laporan.php
+++ app/Controllers/Admin/Laporan.php
@@ -3,14 +3,49 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Models\TransaksiModel;
 
 class Laporan extends BaseController
 {
+    protected $trans;
+
+    public function __construct() {
+        $this->trans = new TransaksiModel();
+    }
+
     public function index(): string
     {
+        session();
+        $lap = $this->trans
+        ->join('siswa', 'siswa.nis = transaksi.nis')
+        ->join('kelas','siswa.kode_kelas = kelas.kode_kelas')
+        ->join('buku','buku.kode_buku = transaksi.kode_buku')
+        ->join('tahun_ajaran', 'siswa.kode_tahun = tahun_ajaran.kode_tahun')
+        ->join('penerbit', 'buku.kode_penerbit = penerbit.kode_penerbit')
+        ->join('rak', 'rak.kode_rak = buku.kode_rak')
+        ->join('jenis_buku','jenis_buku.kode_jenis = buku.kode_jenis')
+        ->findAll();
         $data = [
-            'title' => 'Laporan Transaksi'
+            'title' => 'Laporan Transaksi',
+            'lap' => $lap
         ];
         return view('admin/laporan/tablelaporan', $data);
+    }
+
+    function reset()
+    {
+        if ($this->trans->where('status','kembali')->delete() == true) {
+            session()->setFlashdata('session',[
+                'status'    => 'success',
+                'message'   => 'Laporan berhasil di reset'
+            ]);
+            return redirect()->to(base_url('pustakawan/laporan'));
+        }else{
+            session()->setFlashdata('session',[
+                'status'    => 'error',
+                'message'   => 'Laporan gagal di reset'
+            ]);
+            return redirect()->to(base_url('pustakawan/laporan'));
+        }
     }
 }
--- app/Views/admin/laporan/tablelaporan.php
+++ app/Views/admin/laporan/tablelaporan.php
@@ -31,39 +31,73 @@
               <div class="card-header">
                 <div class="card-title">
                     <h3>Laporan Transaksi</h3>
-                    <a href="#" class="btn btn-primary my-1"><i class="fas fa-solid fa-arrow-right"></i> RESET LAPORAN</a>
+                    <a href="<?= base_url('pustakawan/laporan/reset'); ?>" class="btn btn-primary my-1"><i class="fas fa-solid fa-arrow-right"></i> RESET LAPORAN</a>
                     <!-- <button type="button" class="btn btn-success my-1"><i class="fas fa-solid fa-arrow-down"></i> UNDUH EXCEL</button>
                     <button type="button" class="btn btn-success my-1"><i class="fas fa-solid fa-arrow-up"></i> IMPORT DATA DENDA</button> -->
                 </div>
               </div>
               <!-- /.card-header -->
-              <div class="card-body table-responsive">
+              <div class="card-body">
+                <div class="table-responsive">
                 <table id="example1" class="table table-bordered table-hover">
                   <thead>
                   <tr>
                     <th>NO</th>
+                    <th>NIS</th>
+                    <th>NAMA</th>
+                    <th>KELAS</th>
                     <th>KODE</th>
-                    <th>NAMA DENDA</th>
-                    <th>NOMINAL</th>
+                    <th>JUDUL BUKU</th>
+                    <th>JENIS BUKU</th>
+                    <th>RAK</th>
+                    <th>PENERBIT</th>
+                    <th>STATUS BUKU</th>
+                    <th>TANGGAL PINJAM</th>
+                    <th>TANGGAL KEMBALI</th>
+                    <th>HARI KETERLAMBATAN</th>
+                    <th>DENDA</th>
                   </tr>
                   </thead>
                   <tbody>
+                  <?php $no = 1; foreach($lap as $l) :  ?>
                   <tr>
-                    <td>1</td>
-                    <td>K21</td>
-                    <td>XII MIPA</td>
-                    <td>Rp 12.000</td>
+                    <td><?= $no; ?></td>
+                    <td><?= $l['nis']; ?></td>
+                    <td><?= $l['nama_siswa']; ?></td>
+                    <td><?= $l['nama_kelas']; ?></td>
+                    <td><?= $l['kode_buku']; ?></td>
+                    <td><?= $l['judul_buku']; ?></td>
+                    <td><?= $l['nama_jenis']; ?></td>
+                    <td><?= $l['nama_rak']; ?></td>
+                    <td><?= $l['nama_penerbit']; ?></td>
+                    <td><?= $l['status']; ?></td>
+                    <td><?= $l['pinjam']; ?></td>
+                    <td><?= $l['kembali']; ?></td>
+                    <td><?= $l['terlambat'].' HARI'; ?></td>
+                    <td><?= $l['denda']; ?></td>
                   </tr>
+                  <?php $no++; endforeach; ?>
                   </tbody>
                   <tfoot>
                   <tr>
                     <th>NO</th>
+                    <th>NIS</th>
+                    <th>NAMA</th>
+                    <th>KELAS</th>
                     <th>KODE</th>
-                    <th>NAMA DENDA</th>
-                    <th>NOMINAL</th>
+                    <th>JUDUL BUKU</th>
+                    <th>JENIS BUKU</th>
+                    <th>RAK</th>
+                    <th>PENERBIT</th>
+                    <th>STATUS BUKU</th>
+                    <th>TANGGAL PINJAM</th>
+                    <th>TANGGAL KEMBALI</th>
+                    <th>HARI KETERLAMBATAN</th>
+                    <th>DENDA</th>
                   </tr>
                   </tfoot>
                 </table>
+                  </div>
               </div>
               <!-- /.card-body -->
             </div>

2f6df43800803e0de558559c4a724733122e65df
Imamsaa <imamsafii2222022@gmai.com>
2023-10-05 14:12:08 +0700

    Menambahkan file excel Import Buku

--- app/Views/admin/jenis/tablejenis.php
+++ app/Views/admin/jenis/tablejenis.php
@@ -1,105 +1,107 @@
 <?= $this->extend('admin/template/template'); ?>
- <!-- Content Wrapper. Contains page content -->
- <?= $this->section('content'); ?>
+<!-- Content Wrapper. Contains page content -->
+<?= $this->section('content'); ?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Data Jenis Buku</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="<?= base_url('pustakawan'); ?>">Pustakawan</a></li>
-              <li class="breadcrumb-item">Jenis Buku</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="col-12">
-            <div class="card">
-              <div class="card-header">
-                <div class="card-title">
-                    <h3>DAFTAR JENIS BUKU</h3>
-                  </div>
-                </div>
-                <!-- /.card-header -->
-                <div class="card-body">
-                  <div class="row my-2">
-                    <div class="col-lg-6">
-                      <a href="<?= base_url('pustakawan/jenis/tambah'); ?>" class="btn btn-primary my-1"><i class="fas fa-solid fa-plus"></i> TAMBAHKAN JENIS BUKU</a>
-                      <button type="button" class="btn btn-success my-1"><i class="fas fa-solid fa-arrow-down"></i> UNDUH EXCEL</button>
-                    </div>
-                    <div class="col-lg-4">
-                    <form action="<?= base_url('pustakawan/excel/jenis'); ?>" method="post" id="excel-jenis" enctype="multipart/form-data" class="d-inline">
-                        <div class="custom-file my-1">
-                          <input name="jenis" accept=".xls,.xlsx" type="file" class="custom-file-input" id="jenis">
-                        <label class="custom-file-label" for="rak">Pilih file Excel</label>
-                      </div>
-                    </form>
-                    </div>
-                    <div class="col-lg-2">
-                      <button form="excel-jenis" type="submit" class="btn btn-block btn-success my-1"><i class="fas fa-solid fa-arrow-up"></i> IMPOR EXCEL</button>
-                    </div>
-                  </div>
-                <table id="example1" class="table table-sm table-bordered table-hover">
-                  <thead>
-                  <tr>
-                    <th>NO</th>
-                    <th>KODE JENIS</th>
-                    <th>JENIS BUKU</th>
-                    <th>KODE WARNA</th>
-                    <th>ACTION</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <?php $no = 1; foreach($jenis as $row) : ?>
-                  <tr>
-                    <td><?= $no; ?></td>
-                    <td><?= $row['kode_jenis']; ?></td>
-                    <td><?= $row['nama_jenis']; ?></td>
-                    <td><span style="color:<?= $row['kode_warna']; ?>;"><?= $row['kode_warna']; ?></span></td>
-                    <td>
-                        <a href="<?= base_url('pustakawan/jenis/ubah/'.$row['kode_jenis']); ?>" class="btn btn-sm btn-primary mb-1" ><i class="fas fa-solid fa-pen"></i></a>
-                        <form action="<?= base_url('pustakawan/jenis/delete/'.$row['kode_jenis']); ?>" method="post" class="formdelete d-inline">
-                          <input type="hidden" name="_method" value="DELETE">
-                          <button type="submit" class="btn delete btn-sm btn-danger mb-1" ><i class="fas fa-solid fa-trash"></i></button>
-                        </form>
-                    </td>
-                  </tr>
-                  <?php $no++; endforeach; ?>
-                  </tbody>
-                  <tfoot>
-                  <tr>
-                    <th>NO</th>
-                    <th>KODE JENIS</th>
-                    <th>JENIS BUKU</th>
-                    <th>KODE WARNA</th>
-                    <th>ACTION</th>
-                  </tr>
-                  </tfoot>
-                </table>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
-          </div>
-          <!-- /.col -->
-        </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Data Jenis Buku</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="<?= base_url('pustakawan'); ?>">Pustakawan</a></li>
+            <li class="breadcrumb-item">Jenis Buku</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
   </div>
-  <!-- /.content-wrapper -->
-  <?= $this->endSection(); ?>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-header">
+              <div class="card-title">
+                <h3>DAFTAR JENIS BUKU</h3>
+              </div>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <div class="row my-2">
+                <div class="col-lg-6">
+                  <a href="<?= base_url('pustakawan/jenis/tambah'); ?>" class="btn btn-primary my-1"><i class="fas fa-solid fa-plus"></i> TAMBAHKAN JENIS BUKU</a>
+                  <a href="<?= base_url('excel/FORMAT IMPORT JENIS BUKU.xlsx'); ?>" class="btn btn-success my-1"><i class="fas fa-solid fa-arrow-down"></i> FORMAT IMPOR JENIS</a>
+                </div>
+                <div class="col-lg-4">
+                  <form action="<?= base_url('pustakawan/excel/jenis'); ?>" method="post" id="excel-jenis" enctype="multipart/form-data" class="d-inline">
+                    <div class="custom-file my-1">
+                      <input name="jenis" accept=".xls,.xlsx" type="file" class="custom-file-input" id="jenis">
+                      <label class="custom-file-label" for="rak">Pilih file Excel</label>
+                    </div>
+                  </form>
+                </div>
+                <div class="col-lg-2">
+                  <button form="excel-jenis" type="submit" class="btn btn-block btn-success my-1"><i class="fas fa-solid fa-arrow-up"></i> IMPOR EXCEL</button>
+                </div>
+              </div>
+              <table id="example1" class="table table-sm table-bordered table-hover">
+                <thead>
+                  <tr>
+                    <th>NO</th>
+                    <th>KODE JENIS</th>
+                    <th>JENIS BUKU</th>
+                    <th>KODE WARNA</th>
+                    <th>ACTION</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php $no = 1;
+                  foreach ($jenis as $row) : ?>
+                    <tr>
+                      <td><?= $no; ?></td>
+                      <td><?= $row['kode_jenis']; ?></td>
+                      <td><?= $row['nama_jenis']; ?></td>
+                      <td><span style="color:<?= $row['kode_warna']; ?>;"><?= $row['kode_warna']; ?></span></td>
+                      <td>
+                        <a href="<?= base_url('pustakawan/jenis/ubah/' . $row['kode_jenis']); ?>" class="btn btn-sm btn-primary mb-1"><i class="fas fa-solid fa-pen"></i></a>
+                        <form action="<?= base_url('pustakawan/jenis/delete/' . $row['kode_jenis']); ?>" method="post" class="formdelete d-inline">
+                          <input type="hidden" name="_method" value="DELETE">
+                          <button type="submit" class="btn delete btn-sm btn-danger mb-1"><i class="fas fa-solid fa-trash"></i></button>
+                        </form>
+                      </td>
+                    </tr>
+                  <?php $no++;
+                  endforeach; ?>
+                </tbody>
+                <tfoot>
+                  <tr>
+                    <th>NO</th>
+                    <th>KODE JENIS</th>
+                    <th>JENIS BUKU</th>
+                    <th>KODE WARNA</th>
+                    <th>ACTION</th>
+                  </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<?= $this->endSection(); ?>
